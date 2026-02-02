37919559a4285a76452b54de0a7fc86c44fb03d1
Ratthason Ch. <ratthason.ch@rmuti.ac.th>
2026-02-02 11:50:45 +0700

    Initial commit

---
+++ src/test/java/com/spring_student_activity_api/SpringStudentActivityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_student_activity_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentActivityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
